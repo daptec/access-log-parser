df0ce98b139234cb0a7df435b77518753602462d
Andrey Chabanov <daptec@mail.ru>
2025-02-24 16:52:54 +0300

    Логика добавления файла

--- src/main/java/com/stepup/Main.java
+++ src/main/java/com/stepup/Main.java
@@ -1,11 +1,23 @@
 package com.stepup;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int counter = 0;
+        while (true) {
+            System.out.println("Введите путь к файлу: ");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (!(fileExists || isDirectory)) {
+                System.out.println("Указанный файл не существует или указанный путь является путем к папке");
+                continue;
+            }
+            counter++;
+            System.out.println("Путь указан верно. \nЭто файл номер: " + counter);
+        }
     }
 }
