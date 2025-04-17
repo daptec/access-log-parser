f132326bc1cbc2e8139eae7885f9c3f415485f8b
Andrey Chabanov <daptec@mail.ru>
2025-04-17 08:02:58 +0300

    New logic

--- src/main/java/com/stepup/Main.java
+++ src/main/java/com/stepup/Main.java
@@ -1,7 +1,34 @@
 package com.stepup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        int N = 10;
+        List<Integer> lst = new ArrayList<>();
 
+        for (int i = 1; i <= N; i++) {
+            lst.add(i);
+        }
+
+        for (int i = 0; i < lst.size(); i+=2) {
+            int temp = lst.get(i);
+            lst.set(i, lst.get(i+1));
+            lst.set(i+1, temp);
+        }
+
+        System.out.println(lst);
     }
+
+    public static void reverse(ArrayList<Integer> intList) {
+        int n = intList.size() - 1;
+        for (int i = 0; i < intList.size() / 2; i++) {
+            int temp = intList.get(i);
+            intList.set(i, intList.get(n - i));
+            intList.set(n - i, temp);
+        }
+    }
+
+
 }
--- src/main/java/com/stepup/Statistics.java
+++ src/main/java/com/stepup/Statistics.java
@@ -2,17 +2,22 @@ package com.stepup;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class Statistics {
 
     private int totalTraffic;
     private LocalDateTime minTime;
     private LocalDateTime maxTime;
+    private final Set<String> existingPages;
+    private final Map<String, Integer> osCounter;
 
     public Statistics() {
         this.totalTraffic = 0;
         this.minTime = LocalDateTime.MAX;
         this.maxTime = LocalDateTime.MIN;
+        this.existingPages = new HashSet<>();
+        this.osCounter = new HashMap<>();
     }
 
     public void addEntry(LogEntry logEntry) {
@@ -26,6 +31,15 @@ public class Statistics {
         if (logTime.isAfter(maxTime)) {
             maxTime = logTime;
         }
+
+        if (logEntry.getStatusCode() == 200) {
+            existingPages.add(logEntry.getRequestPath());
+        }
+
+        UserAgent userAgent = new UserAgent(logEntry.getUserAgent());
+        String os = userAgent.getOs().name();
+
+        osCounter.put(os, osCounter.getOrDefault(os, 0) + 1);
     }
 
     public double getTrafficRate() {
@@ -36,5 +50,18 @@ public class Statistics {
         }
 
         return (double) totalTraffic / hoursDifference;
+    }
+
+    public List<String> getExistingPages() {
+        return new ArrayList<>(existingPages);
+    }
+
+    public Map<String, Double> getOperatingSystemStats() {
+        Map<String, Double> stats = new HashMap<>();
+        int total = osCounter.values().stream().mapToInt(Integer::intValue).sum();
+        for (Map.Entry<String, Integer> entry : osCounter.entrySet()) {
+            stats.put(entry.getKey(), (double) entry.getValue() / total);
+        }
+        return stats;
     }
 }
